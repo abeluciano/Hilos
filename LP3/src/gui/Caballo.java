564491ab85a163145dc0ebcedb0b51dfd6508664
Abel Luciano Aragon Alvaro <aaragona@ulasalle.edu.pe>
2023-04-15 16:52:39 -0500

    Caballos

--- LP3/src/gui/Caballo.java
+++ LP3/src/gui/Caballo.java
@@ -48,17 +48,4 @@ public class Caballo extends Thread {
 	}
 }
 
-/*public void run() {
-	for ( int i = 0; i <= FIN; i++) {
-		System.out.println("Posicion" + i + ": " + getName());
-		try {
-			sleep((int) (Math.random() * 2000));
-			System.out.println("El caballo " + getName() + " descansa.");
-		} catch (InterruptedException e) {
-			System.out.println(e);
-		}
-	}
-	System.out.println("Fin de la carrera para: " + getName());
-}
-}*/
 
